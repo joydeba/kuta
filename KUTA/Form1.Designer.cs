3d3808acede01ee3cde21e1e9a7fd433c5897b57
joydeba <joydeba@yahoo.com>
2014-08-23 20:09:16 +0600

    first commit with tab

---
+++ KUTA/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace KUTA
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl_main = new System.Windows.Forms.TabControl();
+            this.tabPage_President = new System.Windows.Forms.TabPage();
+            this.tabPage_VicePresident = new System.Windows.Forms.TabPage();
+            this.tabPage_GeneralSecretary = new System.Windows.Forms.TabPage();
+            this.tabPage_JointSecretary = new System.Windows.Forms.TabPage();
+            this.tabPage_Treasurer = new System.Windows.Forms.TabPage();
+            this.tabPage_SocialWalfareCS = new System.Windows.Forms.TabPage();
+            this.tabPage_PublicationSec = new System.Windows.Forms.TabPage();
+            this.tabPage_Member = new System.Windows.Forms.TabPage();
+            this.tabControl_main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl_main
+            // 
+            this.tabControl_main.Controls.Add(this.tabPage_President);
+            this.tabControl_main.Controls.Add(this.tabPage_VicePresident);
+            this.tabControl_main.Controls.Add(this.tabPage_GeneralSecretary);
+            this.tabControl_main.Controls.Add(this.tabPage_JointSecretary);
+            this.tabControl_main.Controls.Add(this.tabPage_Treasurer);
+            this.tabControl_main.Controls.Add(this.tabPage_SocialWalfareCS);
+            this.tabControl_main.Controls.Add(this.tabPage_PublicationSec);
+            this.tabControl_main.Controls.Add(this.tabPage_Member);
+            this.tabControl_main.Location = new System.Drawing.Point(1, 1);
+            this.tabControl_main.Name = "tabControl_main";
+            this.tabControl_main.SelectedIndex = 0;
+            this.tabControl_main.Size = new System.Drawing.Size(1051, 441);
+            this.tabControl_main.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl_main.TabIndex = 0;
+            // 
+            // tabPage_President
+            // 
+            this.tabPage_President.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_President.Name = "tabPage_President";
+            this.tabPage_President.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_President.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_President.TabIndex = 0;
+            this.tabPage_President.Text = "President ";
+            this.tabPage_President.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_VicePresident
+            // 
+            this.tabPage_VicePresident.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_VicePresident.Name = "tabPage_VicePresident";
+            this.tabPage_VicePresident.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_VicePresident.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_VicePresident.TabIndex = 1;
+            this.tabPage_VicePresident.Text = "Vice-President ";
+            this.tabPage_VicePresident.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_GeneralSecretary
+            // 
+            this.tabPage_GeneralSecretary.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_GeneralSecretary.Name = "tabPage_GeneralSecretary";
+            this.tabPage_GeneralSecretary.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_GeneralSecretary.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_GeneralSecretary.TabIndex = 2;
+            this.tabPage_GeneralSecretary.Text = "General Secretary";
+            this.tabPage_GeneralSecretary.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_JointSecretary
+            // 
+            this.tabPage_JointSecretary.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_JointSecretary.Name = "tabPage_JointSecretary";
+            this.tabPage_JointSecretary.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_JointSecretary.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_JointSecretary.TabIndex = 3;
+            this.tabPage_JointSecretary.Text = "Joint Secretary ";
+            this.tabPage_JointSecretary.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_Treasurer
+            // 
+            this.tabPage_Treasurer.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_Treasurer.Name = "tabPage_Treasurer";
+            this.tabPage_Treasurer.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Treasurer.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_Treasurer.TabIndex = 4;
+            this.tabPage_Treasurer.Text = "Treasurer";
+            this.tabPage_Treasurer.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_SocialWalfareCS
+            // 
+            this.tabPage_SocialWalfareCS.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_SocialWalfareCS.Name = "tabPage_SocialWalfareCS";
+            this.tabPage_SocialWalfareCS.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_SocialWalfareCS.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_SocialWalfareCS.TabIndex = 5;
+            this.tabPage_SocialWalfareCS.Text = "Social Walfare & CS";
+            this.tabPage_SocialWalfareCS.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_PublicationSec
+            // 
+            this.tabPage_PublicationSec.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_PublicationSec.Name = "tabPage_PublicationSec";
+            this.tabPage_PublicationSec.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_PublicationSec.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_PublicationSec.TabIndex = 6;
+            this.tabPage_PublicationSec.Text = "Publication Sec.";
+            this.tabPage_PublicationSec.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_Member
+            // 
+            this.tabPage_Member.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_Member.Name = "tabPage_Member";
+            this.tabPage_Member.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Member.Size = new System.Drawing.Size(1043, 415);
+            this.tabPage_Member.TabIndex = 7;
+            this.tabPage_Member.Text = "Member";
+            this.tabPage_Member.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 441);
+            this.Controls.Add(this.tabControl_main);
+            this.Name = "FormMain";
+            this.Text = "Khulna University Teacher Association Election - 2014";
+            this.tabControl_main.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl_main;
+        private System.Windows.Forms.TabPage tabPage_President;
+        private System.Windows.Forms.TabPage tabPage_VicePresident;
+        private System.Windows.Forms.TabPage tabPage_GeneralSecretary;
+        private System.Windows.Forms.TabPage tabPage_JointSecretary;
+        private System.Windows.Forms.TabPage tabPage_Treasurer;
+        private System.Windows.Forms.TabPage tabPage_SocialWalfareCS;
+        private System.Windows.Forms.TabPage tabPage_PublicationSec;
+        private System.Windows.Forms.TabPage tabPage_Member;
+    }
+}
+
